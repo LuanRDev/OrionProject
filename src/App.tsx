f475ae4f8aa5014fcbe623e776732c38f4fb217a
Luan Rocha <luan.dev14@gmail.com>
2023-02-17 02:05:21 -0400

    refac: Atualizacao na importacao de dependencia depreciada

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useRoutes } from 'react-router-dom';
 import router from './router';
 
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { LocalizationProvider } from '@mui/lab';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 
 import { CssBaseline } from '@mui/material';
 import ThemeProvider from './theme/ThemeProvider';
