d10bbb58f9cbccab602dc5c7ec0c09898f80617d
LuanRDev <luan.dev14@gmail.com>
2023-03-02 11:18:48 -0400

    refac: Ajuste no formulário de registrar um participante

--- src/content/applications/RegistrarParticipacao/RegistrarParticipacao.tsx
+++ src/content/applications/RegistrarParticipacao/RegistrarParticipacao.tsx
@@ -7,6 +7,7 @@ import {
   CardContent,
   Checkbox,
   Divider,
+  FormControlLabel,
   Grid,
   List,
   ListItem,
@@ -20,6 +21,8 @@ import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import NewParticipacaoForm from '../../../components/Forms/NewParticipacaoForm';
 import { Evento } from '../../../models/evento';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ReturnEventoTipo } from '../../../components/ReturnEventoTipo';
+import { FormatDate } from '../../../components/FormatDate';
 
 interface PropsEvento {
   Evento: Evento;
@@ -27,8 +30,13 @@ interface PropsEvento {
 
 function RegistrarParticipacao({ Evento }: PropsEvento) {
   const [expanded, setExpanded] = useState<string | false>(false);
+  const [checked, setChecked] = useState(false);
 
-  const handleChange =
+  const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  const handleExpand =
     (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
       setExpanded(isExpanded ? panel : false);
     };
@@ -40,7 +48,7 @@ function RegistrarParticipacao({ Evento }: PropsEvento) {
           <CardContent>
             <Accordion
               expanded={expanded === 'panel1'}
-              onChange={handleChange('panel1')}
+              onChange={handleExpand('panel1')}
             >
               <AccordionSummary
                 aria-controls="panel1a-content"
@@ -51,58 +59,80 @@ function RegistrarParticipacao({ Evento }: PropsEvento) {
               </AccordionSummary>
               <AccordionDetails>
                 <Stack margin={2}>
-                  <Typography variant="h5">
+                  <Typography variant="h6">
                     Código do evento: {Evento?.id}
                   </Typography>
+                  <Typography variant="h6">
+                    Registre sua participação para o{' '}
+                    {ReturnEventoTipo(Evento.tipoEvento)}, pela empresa
+                    {Evento.empresa}.
+                  </Typography>
+                  <Typography variant="h6">
+                    Descrição do evento: {Evento.descricao}
+                  </Typography>
+                  <Typography variant="h6">
+                    Data da realização: {FormatDate(Evento.dataRealizado)}
+                  </Typography>
                 </Stack>
                 <Divider />
-                <Stack margin={2}>
-                  <Typography variant="h5">
-                    Links para acessar diretamente o conteúdo:{' '}
-                  </Typography>
-                  <List>
-                    {Evento.conteudoEventos.map((arquivo) => (
-                      <ListItem>
-                        <ListItemText
-                          primary={arquivo.name}
-                          secondary={arquivo.url}
+                {Evento.conteudoEventos.length > 0 ? (
+                  <>
+                    <Stack margin={2}>
+                      <Typography variant="h5">
+                        Links para acessar diretamente o conteúdo:{' '}
+                      </Typography>
+                      <List>
+                        {Evento.conteudoEventos.map((arquivo) => (
+                          <ListItem>
+                            <ListItemText
+                              primary={arquivo.name}
+                              secondary={arquivo.url}
+                            />
+                          </ListItem>
+                        ))}
+                      </List>
+                    </Stack>
+                    <Divider />
+                    <Stack margin={2}>
+                      <Typography variant={'body1'}>
+                        Abaixo encontre uma visualização do arquivo embutida à
+                        nossa plataforma. Nem todos os arquivos são compatíveis
+                        com a pré-visualização, nesses casos é recomendado abrir
+                        diretamente o link dos arquivos.
+                      </Typography>
+                      <Box height={'15vh'}>
+                        <DocViewer
+                          documents={docs}
+                          pluginRenderers={DocViewerRenderers}
+                          language={'pt'}
                         />
-                      </ListItem>
-                    ))}
-                  </List>
-                </Stack>
-                <Divider />
-                <Stack margin={2}>
-                  <Typography variant={'body1'}>
-                    Abaixo encontre uma visualização do arquivo embutida à nossa
-                    plataforma. Nem todos os arquivos são compatíveis com a
-                    pré-visualização, nesses casos é recomendado abrir
-                    diretamente o link dos arquivos.
-                  </Typography>
-                  <Box height={'15vh'}>
-                    <DocViewer
-                      documents={docs}
-                      pluginRenderers={DocViewerRenderers}
-                      language={'pt'}
-                    />
-                  </Box>
-                </Stack>
+                      </Box>
+                    </Stack>
+                  </>
+                ) : (
+                  ''
+                )}
+
                 <Divider />
                 <Stack marginTop={2}>
-                  <Typography variant="h5">
-                    Declaro que li e consumi todos os conteúdos disponibilizados
-                    neste evento.
-                  </Typography>
                   <Box>
-                    <Checkbox onChange={handleChange('panel2')} />
+                    <FormControlLabel
+                      control={
+                        <Checkbox onChange={handleCheck} checked={checked} />
+                      }
+                      label={`
+                      Declaro que li e consumi todos os conteúdos
+                      disponibilizados neste evento.`}
+                    />
                   </Box>
                 </Stack>
               </AccordionDetails>
             </Accordion>
             <Divider />
             <Accordion
-              expanded={expanded === 'panel2'}
-              onChange={handleChange('panel2')}
+              expanded={checked}
+              onChange={handleExpand('panel2')}
+              disabled={!checked}
             >
               <AccordionSummary
                 aria-controls="panel1a-content"
